684bf19a1e7876c616def86dbdf61b06ff6e9545
anouri <ahmad.nouri@de.ibm.com>
2014-06-27 11:03:47 +0200

    chnage Copyright statement

--- DbLoader/scripts/Configuration.pm
+++ DbLoader/scripts/Configuration.pm
@@ -1,8 +1,5 @@
-# IBM Confidential
-# OCO Source Materials
-# 
-# (C) Copyright IBM Corp. 2011, 2013
-# The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+# Copyright (C) 2013-2014, International Business Machines Corporation  #
+# All Rights Reserved                                	               #
 
 package Configuration;
 
