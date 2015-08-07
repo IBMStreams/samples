4672160cb32b1cc56f1d029a824631087748927f
KIRSTEN W. HILDRUM <hildrum@us.ibm.com>
2015-08-07 11:33:46 -0400

    Fix bug.  Add source-and-header separate example

--- NativeFunctions/com.ibm.streamsx.aesv2/impl/src/opensslwrapper.cpp
+++ NativeFunctions/com.ibm.streamsx.aesv2/impl/src/opensslwrapper.cpp
@@ -1,11 +1,10 @@
-#ifndef OPENSSLWRAPPER_H
-#define OPENSSLWARPPER_H
 #include <openssl/evp.h>
-#include <SPL/Runtime/Type/Blob.h>
+#include "opensslwrapper.h"
+#include <iostream>
 
 #define BYTES_PER_BLOCK 16
-namespace openssl_for_spl {
 
+namespace openssl_for_spl {
 void crypto(const unsigned char * key, const unsigned char * input, int len, unsigned char* output,int * outputLen, bool encrypt) {
 
 	EVP_CIPHER_CTX ctx;
@@ -18,36 +17,33 @@ void crypto(const unsigned char * key, const unsigned char * input, int len, uns
 		return;
 	}
 	*outputLen=tmpLen;
-	if (!EVP_CipherFinal_ex(&ctx,output+tmpLen,&tmpLen)) {
+	int finalBytes = 0;
+	if (!EVP_CipherFinal_ex(&ctx,output+tmpLen,&finalBytes)) {
 		*outputLen=0;
 		return;
 	}
-	*outputLen += tmpLen;
+	*outputLen += finalBytes;
 	EVP_CIPHER_CTX_cleanup(&ctx);
 
 }
 
+
 SPL::blob aesencrypt(const SPL::blob &key, const SPL::blob& input_data) {
-	SPLAPPTRC(L_DEBUG,"Encrypt input size " << input_data.getSize(),"opensslwrapper");
 	int outputLen = 0;
 	const int len = input_data.getSize();
-	unsigned char  output[len+BYTES_PER_BLOCK];
+	unsigned char  output[len + BYTES_PER_BLOCK];
 	crypto(key.getData(),input_data.getData(),input_data.getSize(),&(output[0]),&outputLen,true);
-    SPL::blob myblob(output,outputLen);;
-    SPLAPPTRC(L_DEBUG,"Encrypt output size " << outputLen,"opensslwrapper");
+	SPL::blob myblob(output,outputLen);;
     return myblob;
 }
 
 SPL::blob aesdecrypt(const SPL::blob& key, const SPL::blob &input_data) {
-	SPLAPPTRC(L_DEBUG,"Decrypt input size " << input_data.getSize(),"opensslwrapper");
 	int outputLen = 0;
 	const int len = input_data.getSize();
+	// allocate a little extra space
 	unsigned char output[len+BYTES_PER_BLOCK];
 	 crypto(key.getData(),input_data.getData(),input_data.getSize(),&(output[0]), &outputLen,false);
 	SPL::blob myblob(output,outputLen);
-	SPLAPPTRC(L_DEBUG,"Decrypt output size " << outputLen,"opensslwrapper");
 	return myblob;
 }
 }
-
-#endif
