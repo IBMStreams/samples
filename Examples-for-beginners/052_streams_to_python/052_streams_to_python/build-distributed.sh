64219b244c3dc06bcc4a74529a37d36b25da7dd2
Natasha D'Silva <ndsilva@ca.ibm.com>
2017-04-17 12:35:44 -0400

    delete obsolete samples

--- Examples-for-beginners/052_streams_to_python/052_streams_to_python/build-distributed.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ./output/python.wrapper.example.streams_to_python/Distributed
-sc -a -M python.wrapper.example::streams_to_python --data-directory=data --output-directory=./output/python.wrapper.example.streams_to_python/Distributed -x -I$(python -c 'import distutils.sysconfig as sc; print sc.get_python_inc()')
--- Examples-for-beginners/052_streams_to_python/052_streams_to_python/run-standalone.sh
+++
@@ -1,3 +0,0 @@
-./output/python.wrapper.example.streams_to_python/Standalone/bin/standalone 
-
-echo "****** You can check the results from this run inside this file: data/UrlToIpAddress-Result.csv ******"
