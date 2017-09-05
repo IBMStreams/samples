566bbd4f7a2174977ff957080b1ea9917705358b
natashadsilva <ndsilva@ca.ibm.com>
2017-09-05 15:39:45 -0400

    add samples for IoT and Edgent recipe and cheat sheet

--- IoT/ReadEdgentEvents/python/StreamsPythonAndEdgent/mysa_creds.py
+++ IoT/ReadEdgentEvents/python/StreamsPythonAndEdgent/mysa_creds.py
@@ -7,21 +7,8 @@ from streamsx.topology.context import *
 
 def get_sa_credentials():
     #Paste the credentials you got from the dashboard here
-    #credentials = {}
+    credentials = {}
 
-    credentials =  { "password": "d44351fe-f4e4-46bd-b7e9-2aef1330243e",
-      "rest_port": "443",
-      "bundles_path": "/jax-rs/bundles/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64",
-      "resources_path": "/jax-rs/resources/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64",
-      "stop_path": "/jax-rs/streams/stop/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64",
-      "rest_host": "streams-app-service.ng.bluemix.net",
-      "size_path": "/jax-rs/streams/size/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64",
-      "jobs_path": "/jax-rs/jobs/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64",
-      "start_path": "/jax-rs/streams/start/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64",
-      "rest_url": "https://streams-app-service.ng.bluemix.net",
-      "userid": "2eedcfc9-8abf-41a5-9e89-a53a25fa5768",
-      "status_path": "/jax-rs/streams/status/service_instances/f734936a-520b-48d8-8a3c-b5d334199979/service_bindings/73be4481-16aa-4ea6-a9cf-4e9e241e4e64"
-    }
     """ #should look like this:
       credentials = {  "password": "212358pp8",
       "rest_port": "443",
@@ -40,7 +27,7 @@ def get_sa_credentials():
     return json.dumps(credentials)
 
 def get_service_name():
-    return "sa_nd" #Change this to your Streaming Analytics service name..so in our screenshot above it would be "test1"
+    return "MyServiceName" #Change this to your Streaming Analytics service name..so in our screenshot above it would be "test1"
 
 def get_config_object():
     _creds = get_sa_credentials()
