312493dbbe8e172eeab2f00d75fec6ef0c6afa88
natashadsilva <ndsilva@ca.ibm.com>
2020-04-27 10:49:38 -0400

    Update ZipServer.py

--- ZipServer.py
+++ ZipServer.py
@@ -15,7 +15,7 @@ class DownloadZipHandler(Handler):
 
     def redirect(self, reason):
         #redirect this request to the main samples page.
-        print "Ignoring request %s, reason: %s " % (self.path, reason)
+        print ("Ignoring request %s, reason: %s " % (self.path, reason))
         self.send_response(301)
         self.send_header("Location", "https://ibmstreams.github.io/samples")
         self.end_headers()
@@ -26,24 +26,24 @@ class DownloadZipHandler(Handler):
     def is_valid_path(self):
         valid = False
         arg_index = self.path.find('?')
-        print "2. Checking arguments "
+        print ("2. Checking arguments ")
         if arg_index >= 0:
             args = cgi.parse_qs(self.path[arg_index+1:])
 
             if ("get" in args):
                 raw_path = args["get"][0]
-                print "3. requested path: " + raw_path
+                print ("3. requested path: " + raw_path)
                 path=urllib.unquote(raw_path)
                 directory = os.getcwd() + "/samples/" + path+"/"
-                print "4. Checking for " + directory
+                print ("4. Checking for " + directory)
                 if os.path.exists(directory):#"samples/" + path):
                     if "." in path or ".." in path or "~" in path or "*" in path:
-                        print "invalid characters in path"
+                        print ("invalid characters in path")
                         return False
                     else:
                         return path
                 else:
-                    print path + " is not in samples dir. "
+                    print (path + " is not in samples dir. ")
         return valid
     def do_GET(self):
         '''
@@ -52,9 +52,9 @@ class DownloadZipHandler(Handler):
         # Parse out the arguments.
         # The arguments follow a '?' in the URL. Here is an example:
         #   http://example.com?arg1=val1
-        print "1. got request " + self.path
+        print ("1. got request " + self.path)
         return_value = self.is_valid_path()
-        print "5. is_valid_path return value: " + str(return_value)
+        print ("5. is_valid_path return value: " + str(return_value))
         if return_value is False:
             self.redirect("6. Path does not exist.")
         else:
@@ -64,16 +64,16 @@ class DownloadZipHandler(Handler):
             zipName = "samples/" + path + "/" + folders[-1] +".zip"
             baseName=folders[-1]+".zip"
             if os.path.exists(zipName):
-                print "6. File exists, opening"
+                print ("6. File exists, opening")
                 f = open(zipName,"rb")
                 self.send_response(200)
-                print "7. Sending header"
+                print ("7. Sending header")
                 self.send_header('Content-type',    'application/zip')
                 self.send_header('Content-Disposition','attachment; filename="'+baseName+'"')
                 self.end_headers()
                 self.wfile.write(f.read())
                 f.close()
-                print "8. Handled request for " + self.path
+                print ("8. Handled request for " + self.path)
             else:
-                print "6. Ignoring request for " + zipName + " because exists : " + str(os.path.exists(zipName))
+                print ("6. Ignoring request for " + zipName + " because exists : " + str(os.path.exists(zipName)))
                 self.redirect("No zip file with name " + zipName+ " within folder")
