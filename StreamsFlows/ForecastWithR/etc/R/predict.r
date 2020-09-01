a80e36287be57c95fa461d4f4f73203965815ace
natashadsilva <ndsilva@ca.ibm.com>
2020-09-01 12:33:58 -0400

    Move score with R sample to its own repo

--- StreamsFlows/ForecastWithR/etc/R/predict.r
+++
@@ -1,4 +0,0 @@
-# this makes sure the model  always returns a number around 5
-# num_unique_users and total_users_counted must be mapped to Streams attributes using the streamAttributes and 
-# rObjects parameters in the RScript operator 
-predictedCount<-(score(num_unique_users, total_users_counted)  + runif(1,0.1, 0.6))
